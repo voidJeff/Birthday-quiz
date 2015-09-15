74e161cc7a35477fc0a11ada7aadf3ea100406e6
voidJeff <qy120838@126.com>
2015-09-15 15:01:23 -0400

    Updated from Brython Server: September 15, 2015 3:01:23 PM EDT

--- birthday.py
+++ birthday.py
@@ -1,7 +1,7 @@
 """
 birthday.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Jeff
+Credit: None
 Assignment:
 
 Your program will ask the user the following questions, in this order:
@@ -31,3 +31,14 @@ Example Session
   And the day? 11
   Eric, you are a fall baby of the stone age.
 """
+
+from datetime import datetime
+from calendar import month_name
+todaymonth = datetime.today().month
+todaydate = datetime.today().day
+
+name = input("Hello, what is your name? ")
+month_ = input("Hi Eric, what was the name of the month you were born in? ")
+year = int(input("And what year were you born in, {0}? ".format(name)))
+date = int(input("And the day? "))
+month = month_name[todaymonth]
