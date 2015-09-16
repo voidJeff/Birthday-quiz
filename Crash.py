9f15af1452f5d5538a59a0dc642be65c1f987807
voidJeff <qy120838@126.com>
2015-09-16 14:05:05 -0400

    Create Crash.py

---
+++ Crash.py
@@ -0,0 +1,8 @@
+bool_ = input("Would you like to crash the browser: ")
+if bool_ == "Yes" or bool_ == "yes" or bool_ == "Yep" or bool_ == "yep":
+    while 0 == 0:
+        print("CRASHING!CRASHING!CRASHING!CRASHING!CRASHING!CRASHING!CRASHING!CRASHING!CRASHING!CRASHING!CRASHING!")
+elif bool_ == "No" or bool_ == "Nope" or bool_ == "no" or bool_ == "nope":
+    print("Alright. Enjoy your boring life:)")
+else:
+    print("It's a yes or no question. Choose again.")
